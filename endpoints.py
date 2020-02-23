f03d6445237c5cdee8afe9b0736c5d974c7eb124
Tommi <tnv8@wp.pl>
2020-02-23 14:12:47 +0100

    test

---
+++ endpoints.py
@@ -0,0 +1,12 @@
+base_url = 'http://18.184.234.77:8080'
+
+# ping controller
+ping = f'{base_url}/ping'
+
+# calculator controller
+calculator_add = f'{base_url}/add'
+
+# accounts controller
+accounts = f'{base_url}/accounts'
+accounts_create = f'{base_url}/accounts/create'
+accounts_delete = f'{base_url}/accounts/delete'
