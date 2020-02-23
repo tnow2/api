fc2a441a6b67dc0ff569515fc63ea6cdad19b3a8
Tommi <tnv8@wp.pl>
2020-02-23 12:10:15 +0100

    test

--- tests/test_accounts.py
+++ tests/test_accounts.py
@@ -1,12 +1,11 @@
 import requests
+import endpoints
 from faker import Faker
-
-base_url = 'http://18.184.234.77:8080'
 fake = Faker()
 
 
 def test_get_accounts_list():
-    response = requests.get(f'{base_url}/accounts')
+    response = requests.get(endpoints.accounts)
     assert response.status_code == 200
 
     response_dict = response.json()
@@ -17,14 +16,19 @@ def test_get_accounts_list():
 
 
 def test_create_account():
+    expected_name = _create_account()
+
+    list_params = {'account': expected_name}
+    filtered_list_response = requests.get(endpoints.accounts, params=list_params)
+    assert filtered_list_response.status_code == 200
+    assert expected_name in filtered_list_response.text
+
+
+def _create_account():
     random_name = fake.uuid4()
     body = {
         "name": random_name
     }
-    create_account_response = requests.put(f'{base_url}/accounts/create', json=body)
+    create_account_response = requests.put(endpoints.accounts_create, json=body)
     assert create_account_response.status_code == 201
-
-    list_params = {'account': random_name}
-    filtered_list_response = requests.get(f'{base_url}/accounts', params=list_params)
-    assert filtered_list_response.status_code == 200
-    assert random_name in filtered_list_response.text
+    return random_name
